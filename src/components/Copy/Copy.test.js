e4dc02b0ddb5f39c1211b041281d6e0245926dc0
Grayson Gilmore <gilmoreg@live.com>
2017-06-07 20:59:34 -0500

    Remove unused tests

--- src/components/Copy/Copy.test.js
+++ src/components/Copy/Copy.test.js
@@ -2,8 +2,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Copy from './Copy';
+import Condictor from '../Repos/Condictor';
 
 it('renders without crashing', () => {
   const section = document.createElement('section');
-  ReactDOM.render(<Copy />, section);
+  ReactDOM.render(<Copy copy={<Condictor />} />, section);
 });
