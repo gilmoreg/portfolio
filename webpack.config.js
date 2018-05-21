c0ee0cec6e26dbc159ad0a9075fc779389c35cc5
Grayson Gilmore <gilmoreg@live.com>
2018-05-20 17:53:07 -0700

    Make progress on webpack migration

--- webpack.config.js
+++ webpack.config.js
@@ -3,21 +3,24 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
+  entry: './src/assets/scripts/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
   },
   module: {
     rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
       { test: /\.pug$/, use: 'pug-loader' },
-      // {
-      //     test: /\.(png|jpg|gif)$/,
-      //     use: [
-      //       {
-      //         loader: 'file-loader',
-      //         options: {}
-      //       }
-      //     ]
-      //   }
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}
+          }
+        ]
+      }
     ]
   },
   plugins: [
